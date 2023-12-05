9a9eb54469dd23f75fe6a7ab7861714c185ec7ef
Rob <robbiejohnwallace@yahoo.co.uk>
2023-12-05 09:42:28 +0000

    part 1 finished.

--- src/days/day_5/mod.rs
+++ src/days/day_5/mod.rs
@@ -6,8 +6,8 @@ use std::io::{self};
 use std::cmp::Ordering;
 
 struct Input {
-    seeds: Vec<u32>,
-    soil_to_soil: Vec<MapElement>,
+    seeds: Vec<u64>,
+    seed_to_soil: Vec<MapElement>,
     soil_to_fertilizer: Vec<MapElement>,
     fertilizer_to_water: Vec<MapElement>,
     water_to_light: Vec<MapElement>,
@@ -18,9 +18,9 @@ struct Input {
 
 #[derive(Eq)]
 struct MapElement {
-    des_range_start: u32,
-    src_range_start: u32,
-    length: u32
+    des_range_start: u64,
+    src_range_start: u64,
+    length: u64
 }
 
 
@@ -37,8 +37,42 @@ fn part1(file_name: &str) -> Result<u32, &'static str> {
     let input = parse_input(file_name)?;
 
     print_input(&input);
+    let mut min = std::u64::MAX;
+    for s in &input.seeds {
+        let n = get_soil_number(&input, *s);
+        println!("s{} n{}", *s, n);
+        if n < min {
+            min = n;
+        }
+    }
 
-    Ok(0)
+    Ok(min as u32)
+}
+
+
+fn get_soil_number(input: &Input, seed: u64) -> u64 {
+    let soil = get_mapping(seed, &input.seed_to_soil);
+    let fert = get_mapping(soil, &input.soil_to_fertilizer);
+    let water = get_mapping(fert, &input.fertilizer_to_water);
+    let light = get_mapping(water, &input.water_to_light);
+    let temp = get_mapping(light, &input.light_to_temerature);
+    let hum = get_mapping(temp, &input.temerature_to_humidity);
+    let loc = get_mapping(hum, &input.humidity_to_location);
+    return loc;
+}
+
+fn get_mapping(n: u64, map: &Vec<MapElement>) -> u64 {
+    for el in map {
+        if n < el.src_range_start {
+            return n;
+        }
+
+        if n < el.src_range_start + el.length {
+            return el.des_range_start + (n - el.src_range_start)
+        }
+    }
+
+    return n;
 }
 
 
@@ -69,7 +103,7 @@ fn parse_input(file_name: &str) -> Result<Input, &'static str> {
         return Err("Failed to parse seeds");
     };
 
-    let Some(soil_to_soil) = parse_map(&mut lines) else {
+    let Some(seed_to_soil) = parse_map(&mut lines) else {
         return Err("Failed to parse soil to soil");
     };
 
@@ -99,7 +133,7 @@ fn parse_input(file_name: &str) -> Result<Input, &'static str> {
  
     Ok(Input {
         seeds,
-        soil_to_soil,
+        seed_to_soil,
         soil_to_fertilizer,
         fertilizer_to_water,
         water_to_light,
@@ -112,8 +146,6 @@ fn parse_input(file_name: &str) -> Result<Input, &'static str> {
 
 fn parse_map(lines: &mut io::Lines<io::BufReader<File>>) -> Option<Vec<MapElement>> {
     let mut elements = Vec::new();
-
-    println!("a");
 
     while let Some(l) = lines.next() {
         let Ok(line) = l else {
@@ -131,6 +163,8 @@ fn parse_map(lines: &mut io::Lines<io::BufReader<File>>) -> Option<Vec<MapElemen
             }
         }
     }
+
+    elements.sort();
 
     if elements.len() == 0 {
         None 
@@ -150,7 +184,7 @@ fn parse_map_element(line: &str) -> Option<MapElement> {
             break;
         };
 
-        nums.push(n as u32);
+        nums.push(n as u64);
     }
 
     return if nums.len() != 3 {
@@ -165,7 +199,7 @@ fn parse_map_element(line: &str) -> Option<MapElement> {
 }
 
 
-fn parse_seeds(lines: &mut io::Lines<io::BufReader<File>>) -> Option<Vec<u32>>{
+fn parse_seeds(lines: &mut io::Lines<io::BufReader<File>>) -> Option<Vec<u64>>{
     let Ok(line) = lines.next()? else {
         return None;
     };
@@ -178,7 +212,7 @@ fn parse_seeds(lines: &mut io::Lines<io::BufReader<File>>) -> Option<Vec<u32>>{
             break;
         };
 
-        seeds.push(n as u32);
+        seeds.push(n as u64);
     }
 
     Some(seeds)
@@ -186,10 +220,34 @@ fn parse_seeds(lines: &mut io::Lines<io::BufReader<File>>) -> Option<Vec<u32>>{
 
 fn print_input(input: &Input) {
     println!("Seeds:");
+    print!("    ");
 
     for s in &input.seeds {
         print!("{} ", s);
     }
 
-    println!("")
+    println!("");
+
+    println!("seed_to_soil: ");
+    print_mapping(&input.seed_to_soil);
+    println!("soil_to_fertilizer: ");
+    print_mapping(&input.soil_to_fertilizer);
+    println!("fertilizer_to_water: ");
+    print_mapping(&input.fertilizer_to_water);
+    println!("water_to_light: ");
+    print_mapping(&input.water_to_light);
+    println!("light_to_temerature: ");
+    print_mapping(&input.light_to_temerature);
+    println!("temerature_to_humidity: ");
+    print_mapping(&input.temerature_to_humidity);
+    println!("humidity_to_location: ");
+    print_mapping(&input.humidity_to_location);
+}
+
+fn print_mapping(input: &Vec<MapElement>) {
+    for el in input {
+        println!("    s{} d{} l{}", 
+            el.src_range_start, el.des_range_start, el.length
+        );
+    }
 }
